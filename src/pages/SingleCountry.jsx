d103ad925de3d0f626984713cbbd52ca03d70ea4
Nichita Condrea <N00233189@iadt.ie>
2025-11-02 22:20:50 +0000

    Feature: added currency exchange rate fetching+displaying, tidied up the singlecountry loading state, animations maybe needed

--- src/pages/SingleCountry.jsx
+++ src/pages/SingleCountry.jsx
@@ -6,6 +6,7 @@ export default function SingleCountry() {
     const { name } = useParams();
     const [country, setCountry] = useState(null);
     const [weather, setWeather] = useState(null);
+    const [exchangeRate, setExchangeRate] = useState(null);
 
     //Country fetch
     useEffect(() => {
@@ -33,22 +34,47 @@ export default function SingleCountry() {
         }
     }, [country]);
 
-    if (country === null || weather === null) {
-        return <p>Loading...</p>;
+    //Currency exchange fetch
+    useEffect(() => {
+        if (country && country.currencies) {
+            const currencyCode = Object.keys(country.currencies)[0];
+            axios
+                .get(
+                    `https://api.exchangerate.host/convert?from=EUR&to=${currencyCode}&amount=1&access_key=387d05a7415fb53062718282321e3f3c`
+                )
+                .then((ExchangeResponse) => {
+                    console.log("exchange response:", ExchangeResponse.data);
+                    setExchangeRate(ExchangeResponse.data);
+                })
+                .catch((error) => console.error("Exchange api error:", error));
+        }
+    }, [country]);
+
+    if (country === null || weather === null || exchangeRate === null) {
+        return (
+            <div className="hero bg-base-200 min-h-[60vh]">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    {/* <div className="skeleton min-w-[80vh] min-h-[60vh]"></div> */}
+                </div>
+            </div>
+        );
     }
+
+    const currencyCode = Object.keys(country.currencies)[0];
+    const currencyInfo = country.currencies[currencyCode];
 
     // console.log(country.currencies["EGP"]);
     // console.log(Object.keys(country.currencies));
 
-    let currencies = Object.keys(country.currencies).map((currency) => {
-        return (
-            <p>
-                <b>Code: </b> {currency} <br />
-                <b>Name: </b> {country.currencies[currency].name} <br />
-                <b>Symbol: </b> {country.currencies[currency].symbol}
-            </p>
-        );
-    });
+    // let currencies = Object.keys(country.currencies).map((currency) => {
+    //     return (
+    //         <p>
+    //             <b>Code: </b> {currency} <br />
+    //             <b>Name: </b> {country.currencies[currency].name} <br />
+    //             <b>Symbol: </b> {country.currencies[currency].symbol}
+    //         </p>
+    //     );
+    // });
 
     return (
         <>
@@ -64,11 +90,12 @@ export default function SingleCountry() {
                             {country.name.common}
                         </h1>
                         <p className="py-6">
-                            {country.name.common}{" "}
-                            {country.landlocked ? "is " : "is not "} a
-                            landlocked country in {country.region} which has a
-                            population of {country.population} people, located
-                            in the {country.timezones} timezone.
+                            {country.name.common} is a{" "}
+                            {country.landlocked ? "landlocked" : "coastal"}{" "}
+                            country located in {country.region}, with a
+                            population of {country.population.toLocaleString()}{" "}
+                            people. {country.name.common} is located in the{" "}
+                            {country.timezones[0]} timezone.
                         </p>
                         {/* <button className="btn btn-primary">Get Started</button> */}
                     </div>
@@ -103,7 +130,14 @@ export default function SingleCountry() {
                     <h2 className="text-4xl font-bold flex justify-center mb-8">
                         Currency stats
                     </h2>
-                    <p>basic currency text</p>
+                    <p className="mb-2">
+                        The currency in {country.name.common} is the{" "}
+                        {currencyInfo.name} ({currencyCode})
+                    </p>
+                    <p>
+                        1 Euro converts to {exchangeRate.result}{" "}
+                        {exchangeRate.query.to}
+                    </p>
                 </div>
             </div>
             {/* <img src={country.flags.png} />
